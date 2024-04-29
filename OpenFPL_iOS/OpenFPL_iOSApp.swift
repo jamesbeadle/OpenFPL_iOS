1142b1b3d7134234f9e67dad74404e9da2ca41d8
James Beadle <jamesbeadle1989@gmail.com>
2024-04-29 14:27:52 +0100

    Initial Commit

---
+++ OpenFPL_iOS/OpenFPL_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenFPL_iOSApp.swift
+//  OpenFPL_iOS
+//
+//  Created by James Beadle on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct OpenFPL_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
